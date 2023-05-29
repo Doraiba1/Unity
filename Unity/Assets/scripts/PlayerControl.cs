82b5e0aa776f90200fce05f69724acab927e2c57
liuyuan <1715253430@qq.com>
2023-05-29 20:52:29 +0800

    添加了，人物部分脚本和动画

--- Unity/Assets/scripts/PlayerControl.cs
+++ Unity/Assets/scripts/PlayerControl.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private Animation ani;
+    private Rigidbody2D rBody;
+    private SpriteRenderer sr;
+    public int Hp = 100;
+    private bool isGround = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animation>();
+        rBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Hp <= 0) { return; }
+        float horizontal = Input.GetAxis("Hroizontal");
+        if (horizontal != 0)
+        {
+            transform.Translate(transform.right * 1 * Time.deltaTime * horizontal);
+            if (horizontal < 0)
+            {
+                sr.flipX = true;
+            }
+            if (horizontal > 0)
+            {
+                sr.flipX = false;
+            }
+            
+        }
+        else
+        {
+            
+        }
     }
 }
