424ecfe8d6f61264852ce917ceace8728f9b4042
liuyuan <1715253430@qq.com>
2023-06-07 21:37:33 +0800

    人物脚本

--- Unity/Assets/scripts/PlayerControl.cs
+++ Unity/Assets/scripts/PlayerControl.cs
@@ -5,40 +5,53 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private Animation ani;
+    private Animator ani;
     private Rigidbody2D rBody;
     private SpriteRenderer sr;
     public int Hp = 100;
-    private bool isGround = false;
+    private bool isGround = true;
+    public float MoveSpeed = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        ani = GetComponent<Animation>();
+        ani = GetComponent<Animator>();
         rBody = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        ani.SetFloat("Speed", MoveSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Hp <= 0) { return; }
-        float horizontal = Input.GetAxis("Hroizontal");
+        float horizontal = Input.GetAxis("Horizontal");
+        if(horizontal == 0)
+        {
+            MoveSpeed = 0.0f;
+            ani.SetFloat("Speed", MoveSpeed);
+        }
         if (horizontal != 0)
         {
-            transform.Translate(transform.right * 1 * Time.deltaTime * horizontal);
+            MoveSpeed = 20.0f;
+            ani.SetFloat("Speed", 20.0f);
+            transform.Translate(transform.right * MoveSpeed * Time.deltaTime * horizontal);
+            //Íæ¼Ò×ªÏò
             if (horizontal < 0)
-            {
-                sr.flipX = true;
-            }
-            if (horizontal > 0)
             {
                 sr.flipX = false;
             }
-            
+            if (horizontal > 0)
+            {
+                sr.flipX = true;
+            }
         }
-        else
+        //else
+        //{
+        //    ani.SetFloat("Speed", MoveSpeed);
+        //}
+        if (Input.GetKeyDown(KeyCode.J) && isGround == true)
         {
-            
+            ani.SetTrigger("Is attack");
         }
     }
 }
